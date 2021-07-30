e2bd47ca699b0a0654c23384188b6efeb4c16616
Ronald Mutebi <mutebironaldroninho@gmail.com>
2021-07-30 10:01:55 +0300

    add some extra validations

--- server/Models/pizza.js
+++ server/Models/pizza.js
@@ -13,8 +13,18 @@ db.on('error', console.error.bind(console, 'Pizza Model connection error'))
 
 const PizzaSchema = new Schema({
     _id: { type : ObjectId , unique: 'A Pizza with that id already exists'},
-    name: { type: String, unique: 'A Pizza with the name ({VALUE}) already exists' , required: true},
-    price: { type: Number, required: true }
+    name: { 
+        type: String,
+        unique: 'A Pizza with the name ({VALUE}) already exists' ,
+        required: [true, 'A Pizza must have a name'],
+        minlength: 4,
+        maxlength : 200
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is empty'],
+        min : [1, 'Must be atleast 1']
+    }
 });
 
 PizzaSchema.plugin(require('mongoose-unique-validator'), { type: 'mongoose-unique-validator'});
