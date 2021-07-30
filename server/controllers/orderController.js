ba6c13e782223c6f98a959b5d23e3464bf4c87d1
Ronald Mutebi <mutebironaldroninho@gmail.com>
2021-07-30 16:21:25 +0300

    add ability to make an order

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,5 +1,5 @@
 const OrderItemModel = require('../Models/orderItem');
-
+const PizzaModel = require('../Models/pizza');
 //There should be a "Create Order" function, 
 //the user can select pizza types and amounts, add to the order, 
 //see current total, and place (save) the order.
@@ -13,15 +13,21 @@ const OrderItemModel = require('../Models/orderItem');
 // },
 // totalAmount: { type: Number, required: true }
 module.exports = {
-    createOrder : async (req, res) => {
-        try{
-            let orderItem = new OrderItemModel({
-                quantity: req.body.quantity
-            });
-            let response = await orderItem.save();
-            res.json({ Order: response });
-        }catch(err){
-            res.status(500).json({ error: err.message })
-        }
+    createOrder : async (req, res) => { 
+        PizzaModel.findOne({ _id : req.body.pizza_id })
+            .exec()
+            .then(pizza => {
+                let orderItem = new OrderItemModel({
+                    pizza_id: req.body.pizza_id,
+                    quantity: req.body.quantity,
+                    totalAmount: pizza.price * req.body.quantity
+                });
+                orderItem.save().then( response => {
+                    res.json({ Order: { name: pizza.name, price: pizza.price, quantity: response.quantity, totalAmount: response.totalAmount } });
+                }).catch(err => res.status(500).json({ error: err.message }));  
+            })
+            .catch(err => res.status(500).json({ error: 'The Pizza you want to order is inexistent' }))
+        // 
+        
     }
 }
