ae3b5edf84ef9ecc2a692cfb603aa483f1556f2a
Ronald Mutebi <mutebironaldroninho@gmail.com>
2021-08-03 17:58:32 +0300

    change to async/await

--- server/controllers/orderItemController.js
+++ server/controllers/orderItemController.js
@@ -1,11 +1,16 @@
+const mongoose = require('mongoose')
 const OrderItemModel = require('../Models/orderItem');
 const PizzaModel = require('../Models/pizza');
 
 module.exports = {
     createOrder : async (req, res) => { 
-        PizzaModel.findOne({ _id : req.body.pizza_id })
-            .exec()
-            .then(pizza => {
+        if(!mongoose.Types.ObjectId.isValid(req.body.pizza_id)){
+            return res.status(400).json({ error: 'Enter a valid Pizza id' })
+        }
+
+        try{
+            let pizza = await PizzaModel.findOne({ _id : req.body.pizza_id });
+            if(pizza){
                 let tempTotal = pizza.price * req.body.quantity;
                 let discount = 0;
                 if(tempTotal > 50 && tempTotal < 100){
@@ -18,20 +23,22 @@ module.exports = {
                     quantity: req.body.quantity,
                     totalAmount: tempTotal - discount
                 });
-                orderItem.save().then( response => {
-                    res.status(201).json({
-                        Order: { 
-                            name: pizza.name, 
-                            price: pizza.price, 
-                            quantity: response.quantity, 
-                            totalAmount: response.totalAmount,
-                            discount
-                        } 
-                    });
-                }).catch(err => res.status(500).json({ error: err.message }));  
-            })
-            .catch(err => res.status(500).json({ error: 'The Pizza you want to order is inexistent' }))
-        // 
+                let response = await orderItem.save()
+                res.status(201).json({
+                    Order: { 
+                        name: pizza.name, 
+                        price: pizza.price, 
+                        quantity: response.quantity, 
+                        totalAmount: response.totalAmount,
+                        discount
+                    } 
+                });
+            }else {
+                return res.status(400).json({ error: 'The Pizza you want to order is inexistent' })
+            }
+        }catch(err){
+            res.status(400).json({ error: err.message })
+        }
         
     },
     //list of orders
@@ -42,21 +49,26 @@ module.exports = {
         });
     },
     // details of an individual order
-    getOrderDetails: (req, res) => {
-        OrderItemModel.findOne({_id : req.params.id})
-            .exec()
-            .then(order => {
-                PizzaModel.findOne({ _id: order.pizza_id })
-                    .exec()
-                    .then(pizza => {
-                        res.status(200).json({
-                            name: pizza.name,
-                            price: pizza.price,
-                            quantity: order.quantity,
-                            totalAmount: order.totalAmount
-                        })
-                    }).catch(err => res.status(500).json({ error: err.message }));
-            }).catch(err => res.status(500).json({ error: 'The order id passed is inexistent' }));
+    getOrderDetails: async (req, res) => {
+        if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+            return res.status(400).json({ error: 'Enter a valid Order id' });
+        }
+        try{
+            let order = await OrderItemModel.findOne({_id : req.params.id});
+            let pizza = await PizzaModel.findOne({ _id: order.pizza_id });
+            if(pizza){
+                res.status(200).json({
+                    name: pizza.name,
+                    price: pizza.price,
+                    quantity: order.quantity,
+                    totalAmount: order.totalAmount
+                })
+            }else{
+                return res.status(400).json({ error: 'The Pizza is inexistent' })
+            }
+        }catch(err){
+            res.status(500).json({ error: 'The order id passed is incorrect' });
+        }
     },
 
     //future additions include the ability to update an order, delete an order
