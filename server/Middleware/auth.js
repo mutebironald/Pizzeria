21803a1d69fc98ceb006103d9394a453dcdabe49
Ronald Mutebi <mutebironaldroninho@gmail.com>
2021-08-03 17:49:33 +0300

    change to async/await

--- server/Middleware/auth.js
+++ server/Middleware/auth.js
@@ -3,17 +3,15 @@ const jwt = require("jsonwebtoken");
 const config = process.env;
 
 const verifyToken = (req, res, next) => {
-  const token =
+
+  try {
+    const token =
     req.body.authorization || req.query.authorization ||req.headers['authorization'].split(' ')[1];
 
-  if (!token) {
-    return res.status(403).send("A token is required for authentication");
-  }
-  try {
     const decoded = jwt.verify(token, config.SECRET_KEY);
     req.user = decoded;
   } catch (err) {
-    return res.status(401).send("Invalid Token");
+    return res.status(401).send("A valid Token is required");
   }
   return next();
 };
